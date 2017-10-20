076eb1eef4997dc7f41bcd27a4dff79e3ad5b5ba
bryee10 <bryee10@s.sfusd.edu>
2017-10-20 14:20:58 -0700

    final 1

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,25 @@
-class NamedCow
+class NamedCow extends Cow
 {
+	private String cowName;
+	private String mySound;
+	private String myType;
+	public NamedCow(String type,String name,String sound )
+	{
+		cowName = name;
+		mySound = sound;
+		myType = type;
 
+	}
+	public String getName()
+	{
+		return cowName;
+	}
+	public String getSound()
+	{
+		return mySound;
+	}
+	public String getType()
+	{
+		return myType;
+	}
 }
